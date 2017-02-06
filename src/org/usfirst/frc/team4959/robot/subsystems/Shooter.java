8ff7e1a76228122751b52587bb253b1fa5083200
StaleyRobotics <staleyrobotics@gmail.com>
2017-02-06 14:41:53 -0600

    Add files via upload

---
+++ src/org/usfirst/frc/team4959/robot/subsystems/Shooter.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team4959.robot.subsystems;
+
+import org.usfirst.frc.team4959.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Shooter extends Subsystem {
+
+	SpeedController shooterMotor = RobotMap.shooterMotor;
+			
+	private boolean check;
+	private boolean test;
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+    }
+    //Begins running the shooter and sets the speed for it
+    public void runShooter(double speed) {
+    	shooterMotor.set(-speed);
+    }
+    //Turns off the motor for the shooter
+    public void stopShooter() {
+    	shooterMotor.set(0);
+    }
+    
+    public boolean checkForShooting() {
+    	return check;
+    }
+    
+    public void setShooting(boolean isShooting) {
+    	check = isShooting;
+    }
+    
+    public boolean getTest() {
+    	return test;
+    }
+    
+    public void setTest(boolean sTest) {
+    	test = sTest;
+    }
+
+}
+
